fcbdcad20ec911f3a12531756de70f06e14a88ed
Gulshan Jatwa <gulshan.jatwa@avantika.edu.in>
2026-03-26 08:03:29 +0000

    modifyed

--- test.py
+++ test.py
@@ -6,11 +6,24 @@
 #The first and only line of each test case contains one integer 
 #X — the amount of water client drank today.
 
+#""" X ml of water today. Determine if the client followed the doctor's advice or not.
+#Input Format
+
+#The first line contains a single integer 
+#T — the number of test cases. Then the test cases follow.
+#The first and only line of each test case contains one integer 
+#X — the amount of water client drank today.
+
+
 T=int(input())
 for i in range(T):
     x=int(input())
-    if x>=2000:
+    if 10000>x>=2000:
         print("Yes")
+
+    elif x>=10000:
+        print("It's too much")
+
 
     else:
         print("No")
